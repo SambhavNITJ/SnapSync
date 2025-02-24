0e8dc47cadd9f0de0ee36ff2f7f744f21488e98f
Sambhav Agarwal <sambhavagg123@gmail.com>
2025-02-24 23:30:46 +0530

    final

--- client/src/providers/Socket.jsx
+++ client/src/providers/Socket.jsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, useMemo } from "react";
 import { io } from "socket.io-client";
+
 const SocketContext = createContext(null);
 
 export const useSocket = () => {
@@ -8,10 +9,10 @@ export const useSocket = () => {
 }
 
 export const SocketProvider = (props) => {
-    const socket = useMemo(() => io(`${import.meta.env.BACKEND}`), []);
-  return (
-    <SocketContext.Provider value={socket}>
-      {props.children}
-    </SocketContext.Provider>
-  );
+    const socket = useMemo(() => io(import.meta.env.VITE_BACKEND), []);
+    return (
+        <SocketContext.Provider value={socket}>
+          {props.children}
+        </SocketContext.Provider>
+    );
 }
